2b28cfa35a042ead7cadd01b69e6c6f4f23d722e
himanshu <himanshukp324@gmail.com>
2019-10-03 22:34:02 +0530

    First commit

---
+++ Language Family/src/Language.java
@@ -0,0 +1,25 @@
+
+
+class Language{
+	  protected String name;
+	  protected int numSpeakers;
+	  protected String regionsSpoken;
+	  protected String wordOrder;
+	  
+	  public Language(String getName, int getNumSpeakers, String getRegionsSpoken, String getWordOrder){
+	    this.name = getName;
+	    this.numSpeakers = getNumSpeakers;
+	    this.regionsSpoken = getRegionsSpoken;
+	    this.wordOrder = getWordOrder;
+	  }
+	  
+	  public void getInfo(){
+	    System.out.println(name+ " is spoken by "+numSpeakers+"  people mainly in "+regionsSpoken);
+	    System.out.println("The language follows the word order: "+wordOrder);
+	  }
+	  
+	  public static void main(String[] args){
+	    Mayan mayanLanguage = new Mayan("Ki'che'",30000);
+	    mayanLanguage.getInfo();
+	  }
+	}
