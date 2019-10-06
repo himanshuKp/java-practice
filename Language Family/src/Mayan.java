ee8ad4fd26a769b3c84575df83439a7f28a36f06
himanshu <cse10324.sbit@gmail.com>
2019-10-06 13:50:53 +0530

    working fine now

--- Language Family/src/Mayan.java
+++ Language Family/src/Mayan.java
@@ -1,19 +1,16 @@
 
 
 class Mayan extends Language {
-	  protected String name;
-	  protected int numSpeakers;
-	  protected String region = "Central America";
-	  protected String wordOrder = "verb-object-subject";
+	  private static final String region = "Central America";
+	  private static final String wordOrder = "verb-object-subject";
 	  
-	  Mayan(String languageName,int speakers, String region, String wordOrder ){
+	  Mayan(String languageName,int speakers ){
 	    super(languageName,speakers,region,wordOrder);
 	  }
 	  
 	  @Override
 	  public void getInfo() {
-	    System.out.println(this.name+" is spoken by "+this.numSpeakers+" people mainly in Central America.");
-	    System.out.println("The language follows the word order: verb-object-subject");
-	    System.out.println("Fun fact: "+this.name+" is an ergative language.");
+	    super.getInfo();
+	    System.out.println("Fun fact: "+name+" is an ergative language.");
 	  }
 	}
