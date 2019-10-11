473538aa167bc37e7e2b42f5f744a34baeed6881
himanshuKp <cse10324.sbit@gmail.com>
2019-10-11 17:58:51 +0530

    CHALLENGE 2

---
+++ basics/src/megabytesconverter/Main.java
@@ -0,0 +1,12 @@
+package megabytesconverter;
+
+public class Main {
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+    MegaBytesConverter.printMegaBytesAndKiloBytes(2500);
+    MegaBytesConverter.printMegaBytesAndKiloBytes(-1024);
+    MegaBytesConverter.printMegaBytesAndKiloBytes(5000);
+  }
+
+}
