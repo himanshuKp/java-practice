3a54eaca536b0c41f6b4bf2211414a81a520f702
Himanshu Kandpal <cse10324.sbit@gmail.com>
2019-10-23 15:46:00 +0530

    get hold of class object, methods and field in heap memory

--- OOPs/src/com/himanshu/challenge/Vehicle/Main.java
+++ OOPs/src/com/himanshu/challenge/Vehicle/Main.java
@@ -1,14 +1,37 @@
 package com.himanshu.challenge.Vehicle;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 		BMW bmw = new BMW(36);
-		bmw.accelerate(30);
-		bmw.accelerate(36);
-		bmw.accelerate(-56);
-		bmw.changeVelocity(49, 20);
+
+		Class c1 = bmw.getClass();
+		
+		System.out.println(c1);	//get hold of class object created by JVM
+		
+		System.out.println(c1.getName());  //print type of object using c1
+		
+		//get all methods declared in the class in an array
+		Method m[] = c1.getDeclaredMethods();
+		for(Method method:m) {
+			System.out.println(method.getName());
+		}
+		
+		//getting all fields in an array
+		Field f[] = c1.getDeclaredFields();
+		for(Field field:f) {
+			System.out.println(field.getName());
+		}
+		
+//		bmw.accelerate(30);
+//		bmw.accelerate(36);
+//		bmw.accelerate(-56);
+//		bmw.changeVelocity(49, 20);
 	}
 
 }
