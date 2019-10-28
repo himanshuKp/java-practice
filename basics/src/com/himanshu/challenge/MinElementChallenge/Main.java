9ab9e430b7a853b05bd766de8121d3f6a4a65702
Himanshu Kandpal <cse10324.sbit@gmail.com>
2019-10-28 17:32:57 +0530

    find minimum element in array

---
+++ basics/src/com/himanshu/challenge/MinElementChallenge/Main.java
@@ -0,0 +1,51 @@
+package com.himanshu.challenge.MinElementChallenge;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+	static Scanner scan = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Enter the number of elements you need to enter: ");
+		
+		int count = scan.nextInt();
+		
+		int[] myNumbers = readIntegers(count);
+
+		scan.close();
+		findMin(myNumbers);
+		
+	}
+
+	private static void findMin(int[] findMin) {
+		
+		int temp;
+		boolean flag = true;
+		while(flag) {
+			flag = false;
+			for(int i=0;i<findMin.length-1;i++) {
+				if(findMin[i]<findMin[i+1]) {
+					temp = findMin[i];
+					findMin[i] = findMin[i+1];
+					findMin[i+1] = temp;
+					flag = true;
+				}
+			}			
+		}
+		System.out.println("Sorted array: "+Arrays.toString(findMin));
+		System.out.println("Minimum element in array: " +findMin[findMin.length-1]);
+	}
+
+	private static int[] readIntegers(int count) {
+		System.out.println("Enter numbers to sort: ");
+		int[] tempArray = new int[count];
+		for(int i=0;i<count;i++) {
+			tempArray[i] = scan.nextInt();
+		}
+		return tempArray;
+	}
+
+}
