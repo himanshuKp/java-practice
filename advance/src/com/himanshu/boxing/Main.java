6a042cec28a608511b0595dd260b092758617ed9
himanshuKp <cse10324.sbit@gmail.com>
2019-10-31 21:09:28 +0530

    solved bug

--- advance/src/com/himanshu/boxing/Main.java
+++ advance/src/com/himanshu/boxing/Main.java
@@ -3,25 +3,25 @@ package com.himanshu.boxing;
 import java.util.ArrayList;
 
 public class Main {
-  
+
   public static void main(String[] args) {
 //    ArrayList<IntValue> myIntValue = new ArrayList<IntValue>();
-//    myIntValue.add(new IntValue(10));  
+//    myIntValue.add(new IntValue(10));
+//
 //    
-    /*
-     * for(int count=0;count<myIntValue.size();count++) { IntValue myInt =
-     * myIntValue.get(count); System.out.println(myInt.getValue()); }
-     */
-    
-    ArrayList<Integer> myIntBoxingArrayList = new ArrayList<Integer>();
-    for(int count=0;count<myIntBoxingArrayList.size();count++) {
-      myIntBoxingArrayList.add(Integer.valueOf(count));   //autoboxing
+//     for(int count=0;count<myIntValue.size();count++) { IntValue myInt =
+//     myIntValue.get(count); System.out.println(myInt.getValue()); }
+     
+
+    ArrayList<Integer> myIntValue = new ArrayList<Integer>();
+    for(int count=0;count<10;count++) {
+      myIntValue.add(Integer.valueOf(count));   //autoboxing
     }
     
-    for(int count=0;count<myIntBoxingArrayList.size();count++) {
-      System.out.println(count+" --> "+myIntBoxingArrayList.get(count).intValue());   //unboxing
+    for(int count=1;count<myIntValue.size();count++) {
+      System.out.println(count+" --> "+myIntValue.get(count).intValue());   //unboxing
     }
-    
+
   }
 
 }
