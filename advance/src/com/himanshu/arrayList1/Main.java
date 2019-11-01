33d2b0132a85460544f915f48a4a264b0535fe2b
himanshuKp <cse10324.sbit@gmail.com>
2019-11-01 07:03:46 +0530

    arraylist example

--- advance/src/com/himanshu/arrayList1/Main.java
+++ advance/src/com/himanshu/arrayList1/Main.java
@@ -1,15 +1,32 @@
 package com.himanshu.arrayList1;
 
+import java.util.ArrayList;
+
 public class Main {
 
   public static void main(String[] args) {
-    Customer customer = new Customer("Himanshu",32.11);
-    Customer anotherCustomer;
-    anotherCustomer = customer;
-    anotherCustomer.setBalance(10.11);
-    System.out.println("Balance of customer " +customer.getName()+ " is " +customer.getBalance()); //because to save memory anotherCustomer already referred
-    //to the same address of customer after anotherCustomer = customer.
+//    Customer customer = new Customer("Himanshu",32.11);
+//    Customer anotherCustomer;
+//    anotherCustomer = customer;
+//    anotherCustomer.setBalance(10.11);
+//    System.out.println("Balance of customer " +customer.getName()+ " is " +customer.getBalance()); //because to save memory anotherCustomer already referred
+//    //to the same address of customer after anotherCustomer = customer.
     
+    ArrayList<Integer> arrayList = new ArrayList<Integer>();
+    arrayList.add(1);
+    arrayList.add(2);
+    arrayList.add(3);
+    arrayList.add(4);
     
+    for(int i=0;i<arrayList.size();i++) {
+      System.out.println(i+" : "+arrayList.get(i));
+    }
+    
+    //adding element at position 2;
+    arrayList.add(2,5);
+  
+    for(int i=0;i<arrayList.size();i++) {
+      System.out.println(i+" : "+arrayList.get(i));
+    }
   }
 }
