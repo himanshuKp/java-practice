821a4dadc57b54b26ecd2849a67f97eb91fcf83c
himanshuKp <cse10324.sbit@gmail.com>
2019-11-09 00:08:06 +0530

    add song to playlist

--- advance/src/com/himanshu/challenge/linkedList/Albums.java
+++ advance/src/com/himanshu/challenge/linkedList/Albums.java
@@ -1,6 +1,7 @@
 package com.himanshu.challenge.linkedList;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class Albums {
   
@@ -30,5 +31,29 @@ public class Albums {
     }
     return null;
   }
+  
+  //add song to playlist using song track number
+  public boolean addToPlaylist(int trackNumber, LinkedList<Songs> playlist) {
+    int index = trackNumber - 1;    //counting start from 0 hence, -1
+    if((index >= 0) && (index <= this.songs.size())) {
+      playlist.add(this.songs.get(index));
+      return true;
+    }
+    System.out.println("This album does not have a track " +trackNumber);
+    return false;
+  }
+  
+  
+  //add song to playlist  using a song title
+  public boolean addToPlaylist(String title, LinkedList<Songs> playlist) {
+    Songs checkSong = findSong(title);
+    if(checkSong == null) {
+      playlist.add(checkSong);
+      return true;
+    }
+    System.out.println("The song " +title+ " is not in the list.");
+    return false;
+  }
+  
   
 }
