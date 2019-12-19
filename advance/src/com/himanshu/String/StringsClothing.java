f427a91206fb4d11bdb5db0a3aa92938f4e8a7b5
himanshuKp <cse10324.sbit@gmail.com>
2019-12-20 00:06:28 +0530

    basic testing

---
+++ advance/src/com/himanshu/String/StringsClothing.java
@@ -0,0 +1,9 @@
+package com.himanshu.String;
+
+public class StringsClothing extends java.lang.String {
+
+	public void setCharAt(int index, char newChar) {
+		//wont work as String is final class which cannot be subclassed
+	}
+	
+}
