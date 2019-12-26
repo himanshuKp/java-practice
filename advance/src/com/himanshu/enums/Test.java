45ec237619f08978c4c19d28b6d0e534e229d7ff
himanshuKp <cse10324.sbit@gmail.com>
2019-12-26 13:10:26 +0530

    abstract method can be used in enums

--- advance/src/com/himanshu/enums/Test.java
+++ advance/src/com/himanshu/enums/Test.java
@@ -1,17 +1,51 @@
 package com.himanshu.enums;
 
 enum Fish {
-	STAR,GRUPY,GOLD,SHARK;
+	STAR(1000) {
+		@Override
+		void m1() {
+			// TODO Auto-generated method stub
+			System.out.println("Star abstract method");
+		}
+	},GRUPY(3000) {
+		@Override
+		void m1() {
+			// TODO Auto-generated method stub
+			System.out.println("Grupy abstract method");
+		}
+	},GOLD(200) {
+		@Override
+		void m1() {
+			// TODO Auto-generated method stub
+			System.out.println("Gold abstract method");
+		}
+	},SHARK {
+		@Override
+		void m1() {
+			// TODO Auto-generated method stub
+			System.out.println("Shark abstract method");
+		}
+	};
+	int price;
 	Fish(){
+		this.price = 50;
 		System.out.println("Constructor");
 	}
 	//public static void main(String[] args) {
 	//	System.out.println("Enum main method");
 	//}
+	Fish(int price){
+		this.price = price;
+	}
+	abstract void m1();
 }
 class Test{
 	public static void main(String[] args) {
-		Fish f = Fish.GOLD;
-		System.out.println("Hello "+f);
+		Fish[] myFishes = Fish.values();
+		for(Fish myFish:myFishes)
+		{
+			System.out.println("Fish: " +myFish+". Price: " +myFish.price);
+			myFish.m1();
+		}
 	}
 }
