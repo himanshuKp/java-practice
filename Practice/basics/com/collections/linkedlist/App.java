2a1f22d7b2c0b3d523c74d2c963c823c7005f099
Himanshu Kandpal <cse10324.sbit@gmail.com>
2020-06-14 14:16:08 +0530

    comprable

--- Practice/basics/com/collections/linkedlist/App.java
+++ Practice/basics/com/collections/linkedlist/App.java
@@ -1,32 +1,72 @@
 package com.collections.linkedlist;
 
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
+class Names implements Comparable<Names>{
+	
+	String name;
+
+	public Names(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return name;
+	}
+
+	@Override
+	public int compareTo(Names obj) {
+		if (name.length() == obj.name.length()) {
+			return 0;
+		} else if (name.length() < obj.name.length()) {
+			return -1;
+		} else {
+			return 1;
+		}
+	}
+	
+}
+
 public class App {
 
 	public static void main(String[] args) {
-		List<String> countries = new LinkedList<String>();
-		countries.add("India");
-		countries.add("USA");
-		countries.add("Poland");
-		countries.add("Egypt");
-		countries.add("Thailand");
+		List<Names> elements = new LinkedList<Names>();
+		elements.add(new Names("India"));
+		elements.add(new Names("USA"));
+		elements.add(new Names("Poland"));
+		elements.add(new Names("Egypt"));
+		elements.add(new Names("Thailand"));
 		
+				
 		App app = new App();
-		app.printList(countries);
-		System.out.println("******************");
-		countries.sort(null); 	//natural order sorting
-		app.printList(countries);
+		app.printList(elements);
+//		System.out.println("******************");
+//		countries.sort(null); 	//natural order sorting
+//		app.printList(countries);
+//		System.out.println("******************");
+//		Collections.reverse(countries);
+//		app.printList(countries);
+	
+//		Integer a = 1;
+//		Integer b = 1;
+//		
+//		System.out.println(a.compareTo(b));
+		
+		Collections.sort(elements);
+		app.printList(elements);
 	}
 	
-	void printList(List<String> items) {
-		Iterator<String> iterate = items.iterator();
+	void printList(List<Names> items) {
+		Iterator<Names> iterate = items.iterator();
 		while (iterate.hasNext()) {
-			String string = (String) iterate.next();
-			System.out.println("Country: "+string);
+			System.out.println("Country: "+iterate.next());
 		}
 	}
+	
+	
 
 }
