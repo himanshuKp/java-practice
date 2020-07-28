308a7398b3ed8cb44d20c86b9626a2900adb8fa1
Himanshu Kandpal <cse10324.sbit@gmail.com>
2020-07-29 01:45:10 +0530

    multithread

--- Practice/basics/com/multithread/synchronize/method/App.java
+++ Practice/basics/com/multithread/synchronize/method/App.java
@@ -1,66 +1,28 @@
 package com.multithread.synchronize.method;
 
 class Brackets {
-	
-//	preferred way to provide the lock
-	private Object timingLock = new Object();
-	
 	public void generate() {
-		synchronized (timingLock) {
-			for (int i = 1; i < 10; i++) {
-
-				try {
-					Thread.sleep(10);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-
-				if (i < 5) {
-					System.out.print("[");
-				} else {
-					System.out.print("]");
-				}
+		for(int i=1;i<=10;i++) {
+			if(i<=5) {
+				System.out.println("[");
+			}else {
+				System.out.println("]");
 			}
-			System.out.println();
 		}
-
-//		for (int j = 0; j < 10; j++) {
-//			try {
-//				Thread.sleep(20);
-//			} catch (InterruptedException e) {
-//				e.printStackTrace();
-//			}
-//		}
+		System.out.println();
 	}
 }
 
+/**
+ * @author Himanshu Kandpal
+ *
+ */
 public class App {
 
 	public static void main(String[] args) {
 		Brackets bracket = new Brackets();
-		new Thread(new Runnable() {
-			@Override
-			public void run() {
-				long startTime = System.currentTimeMillis();
-				for (int i = 0; i < 5; i++) {
-					bracket.generate();
-				}
-				long endTime = System.currentTimeMillis();
-				System.out.println("Thread1: " + (endTime - startTime));
-			}
-		}).start();
-
-		new Thread(new Runnable() {
-			@Override
-			public void run() {
-				long startTime = System.currentTimeMillis();
-				for (int i = 0; i < 5; i++) {
-					bracket.generate();
-				}
-				long endTime = System.currentTimeMillis();
-				System.out.println("Thread2: " + (endTime - startTime));
-			}
-		}).start();
+		bracket.generate();
+		new Thread().start();
 	}
 
 }
---
+++ Practice/basics/com/multithreading/create/App1.java
@@ -0,0 +1,5 @@
+package com.multithreading.create;
+
+public class App1 {
+
+}
