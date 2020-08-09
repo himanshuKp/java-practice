3072583c5329bb21d4fa59f8f18d75a05d215a39
Himanshu Kandpal <cse10324.sbit@gmail.com>
2020-08-09 05:43:45 +0530

    modelandview

--- Spring MVC setup/WebContent/view/displayName.jsp
+++ Spring MVC setup/WebContent/view/displayName.jsp
@@ -1,5 +1,7 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
+<%@ taglib prefix = "c" uri = "http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix = "fn" uri = "http://java.sun.com/jsp/jstl/functions" %>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,6 +9,15 @@
 <title>Display name</title>
 </head>
 <body>
-<%= request.getAttribute("firstname") %>
+Hello ${firstname}
+<br>
+Date: ${date}
+<br>
+Team list:
+<br>
+<c:forEach var="teamMember" items="${team}">
+${teamMember} 
+<br>
+</c:forEach>
 </body>
 </html>
--- Spring MVC setup/src/org/himanshu/MainController.java
+++ Spring MVC setup/src/org/himanshu/MainController.java
@@ -1,10 +1,15 @@
 package org.himanshu;
 
-import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MainController {
@@ -14,11 +19,19 @@ public class MainController {
 		return "index";
 	}
 	
-	@RequestMapping("displayname")
-	public String displayName(HttpServletRequest request) {
-		String name = request.getParameter("firstName");
-		request.setAttribute("firstname", name);
-		return "displayName";
+	@GetMapping("displayname")
+	public ModelAndView displayName(@RequestParam("firstName") String name) {
+		ModelAndView modelAndView = new ModelAndView("displayName");
+		List<String> names = new ArrayList<String>();
+		names.add("Himanshu");
+		names.add("Pratibha");
+		names.add("Sashi");
+		names.add("Check");
+		Date date = new Date();
+		modelAndView.addObject("date", date);
+		modelAndView.addObject("firstname", name);
+		modelAndView.addObject("team", names);
+		return modelAndView;
 	}
 	
 }
