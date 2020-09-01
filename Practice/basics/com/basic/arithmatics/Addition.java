c2f1c32deaa6d43e43c9eb6b2778b00603543cc3
Himanshu Kandpal <cse10324.sbit@gmail.com>
2020-09-01 11:07:02 +0530

    arithmatic with string

---
+++ Practice/basics/com/basic/arithmatics/Addition.java
@@ -0,0 +1,15 @@
+package com.basic.arithmatics;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		String s1 = "xyz";
+		
+		int x=10,y=20,z=30;
+		
+		System.out.println(s1+x+y+z);
+		
+		System.out.println(x+y+s1+z);
+	}
+	
+}
