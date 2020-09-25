7443a93006c699740bf4334a2501891d1f013943
Himanshu Kandpal <cse10324.sbit@gmail.com>
2020-09-25 23:08:36 +0530

    javascript challenges

---
+++ javascript/script/challenge 2.js
@@ -0,0 +1,15 @@
+function changeColor(){
+  var firstDiv = document.getElementById("div1");
+  var secondDiv = document.getElementById("div2");
+  
+  firstDiv.className="hotpink";
+  secondDiv.className="lightblue";
+}
+
+function changeText(){
+  var firstDiv = document.getElementById("div1");
+  var secondDiv = document.getElementById("div2");
+  
+  firstDiv.innerHTML="hotpink";
+  secondDiv.innerHTML="lightblue";
+}
